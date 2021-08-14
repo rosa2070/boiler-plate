953df73f2c9586751aa7fdc1fe72d798e95a4bae
jaeeunhyun <rosa2070@naver.com>
2021-08-14 14:24:29 +0900

    sdgdfs

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
+
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,13 +16,13 @@ app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jaeeun:abcd1234@boilerplate.8z9g3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!~안녕하세요 ~ '))
+app.get('/', (req, res) => res.send('Hello World!~안녕하세요!!'))
 
 app.post('/register', (req, res) => {
 
