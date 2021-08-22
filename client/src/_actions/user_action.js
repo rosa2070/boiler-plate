d9131f5670c3d704844ef70211935da00bde451d
jaeeunhyun <rosa2070@naver.com>
2021-08-22 23:56:56 +0900

    ghj

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export function loginUser(dataToSubmit) {
+
+    const request = axios.post('/api/users/login', body)
+        .then(response => { response.data })
+
+    return {
+        type: "LOGIN_USER",
+        payload: request
+    }
+}
