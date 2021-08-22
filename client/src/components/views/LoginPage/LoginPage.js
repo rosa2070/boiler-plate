9d1c84113666938e864c1df9752b1af7cd8f6a0d
jaeeunhyun <rosa2070@naver.com>
2021-08-22 23:25:11 +0900

    sdfsd

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+
+            <form>
+                <label>Email</label>
+                <input type="email" value onChange />
+                <label>Password</label>
+                <input type="password" value onChange />
+
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
