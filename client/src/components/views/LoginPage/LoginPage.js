d924ab332c6e7acf5461d56eb432671ff52920d9
jaeeunhyun <rosa2070@naver.com>
2021-08-22 23:56:50 +0900

    fhgf

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import { response } from 'express'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 
 function LoginPage() {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+
+
+    }
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center'
             , width: '100%', height: '100vh'
         }}>
 
-            <form>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
                 <label>Email</label>
-                <input type="email" value onChange />
+                <input type="email" value={Email} onChange={onEmailHandler} />
                 <label>Password</label>
-                <input type="password" value onChange />
+                <input type="password" value={Password} onChange={onPasswordHandler} />
 
                 <br />
                 <button>
